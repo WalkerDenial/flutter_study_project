275202640c3afdee1d4418195ae9e63803a4ac26
Walker Denial <WalkerDenial@qq.com>
2018-07-18 09:32:55 +0800

    update

--- lib/constants/Strings.dart
+++ lib/constants/Strings.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const appName = 'Flutty Study';
+  static const appName = 'Flutter Study';
   static const home = 'Home';
   static const englishListPage = 'Englist List';
   static const favoritePage = 'Favorite List';
